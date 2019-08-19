ec64199942f3d088947680df57c0dfc56df4f65f
Albano Yanes <ajyanreyu@gmail.com>
2019-08-19 15:08:28 +0100

    Define Product class

---
+++ src/main/java/com/shiro/products/model/entity/Product.java
@@ -0,0 +1,23 @@
+package com.shiro.products.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "product")
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double price;
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+}
