db362d06387a40c8f5184cade41a6698cf5f6565
Albano Yanes <ajyanreyu@gmail.com>
2019-08-20 12:01:42 +0100

    Revert "Eureka"

--- src/main/java/com/shiro/products/model/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shiro.products.model.repository;
-
-import com.shiro.products.model.entity.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-}
